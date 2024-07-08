5c838ba56fde717680abb4d01cdf01c5833e793e
Wilson Chan <wilsonchan54@gmail.com>
2024-07-08 19:51:28 -0400

    Added code snippet for utils.ts / added font in tailwind.config.ts / npm installed clsx and tailwind-merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { cn } from '@/lib/utils'
 
 const roboto = Roboto({ 
   subsets: ["latin"], 
@@ -20,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={cn('min-h-screen bg-slate-500 font-roboto antialiased', roboto.variable)}>{children}</body>
     </html>
   );
 }
