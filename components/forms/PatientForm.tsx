15dd6a0db065945d18dface2760a51f298c33cc8
Wilson Chan <wilsonchan54@gmail.com>
2024-09-14 23:46:13 -0400

    Created a function to create a new user in member.actions.ts / created submit handler in PatientForm component (WIP)

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,8 @@ import CustomFormFieldTemplate from "../CustomFormFieldTemplate"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from '../../lib/validation'
+import { createUser } from "@/lib/actions/member.actions"
+import { useRouter } from "next/navigation"
 
 
 export enum FormFieldType {     // ensure type validation
@@ -26,6 +28,7 @@ const PatientForm = () => {
   const iconExampleTwo = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXN3b3JkcyI+PHBvbHlsaW5lIHBvaW50cz0iMTQuNSAxNy41IDMgNiAzIDMgNiAzIDE3LjUgMTQuNSIvPjxsaW5lIHgxPSIxMyIgeDI9IjE5IiB5MT0iMTkiIHkyPSIxMyIvPjxsaW5lIHgxPSIxNiIgeDI9IjIwIiB5MT0iMTYiIHkyPSIyMCIvPjxsaW5lIHgxPSIxOSIgeDI9IjIxIiB5MT0iMjEiIHkyPSIxOSIvPjxwb2x5bGluZSBwb2ludHM9IjE0LjUgNi41IDE4IDMgMjEgMyAyMSA2IDE3LjUgOS41Ii8+PGxpbmUgeDE9IjUiIHgyPSI5IiB5MT0iMTQiIHkyPSIxOCIvPjxsaW5lIHgxPSI3IiB4Mj0iNCIgeTE9IjE3IiB5Mj0iMjAiLz48bGluZSB4MT0iMyIgeDI9IjUiIHkxPSIxOSIgeTI9IjIxIi8+PC9zdmc+`
   const iconExampleThree = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXBob25lIj48cGF0aCBkPSJNMjIgMTYuOTJ2M2EyIDIgMCAwIDEtMi4xOCAyIDE5Ljc5IDE5Ljc5IDAgMCAxLTguNjMtMy4wNyAxOS41IDE5LjUgMCAwIDEtNi02IDE5Ljc5IDE5Ljc5IDAgMCAxLTMuMDctOC42N0EyIDIgMCAwIDEgNC4xMSAyaDNhMiAyIDAgMCAxIDIgMS43MiAxMi44NCAxMi44NCAwIDAgMCAuNyAyLjgxIDIgMiAwIDAgMS0uNDUgMi4xMUw4LjA5IDkuOTFhMTYgMTYgMCAwIDAgNiA2bDEuMjctMS4yN2EyIDIgMCAwIDEgMi4xMS0uNDUgMTIuODQgMTIuODQgMCAwIDAgMi44MS43QTIgMiAwIDAgMSAyMiAxNi45MnoiLz48L3N2Zz4=`
 
+  const router = useRouter()
   const [isLoading, setIsLoading] = useState(false);
 
   // 1. Define your form.
@@ -39,10 +42,22 @@ const PatientForm = () => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof UserFormValidation>) {
+  async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    setIsLoading(true);
+
+    try {
+     const userData = { name, email, phone };
+     const user = await createUser(userData);
+     
+     if(user){
+       router.push(`/patients/${user.$id}/register`)
+     } 
+
+    } catch (error) {
+      console.log(error)
+    }
   }
   
   return (
---
+++ lib/actions/member.actions.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import { ID, Query } from "node-appwrite"
+import { users } from '../appwrite.config'
+
+export const createUser = async (user:CreateUserParams) => {
+  //create auth for appwrite user
+  try {
+    const newUser = await users.create(
+      ID.unique(), 
+      user.email, 
+      user.phone, 
+      user.name, 
+      undefined
+    )
+    
+  } catch (error: any) {
+    if(error && error?.code === 409){
+      const documents = await users.list([
+        Query.equal('email', [user.email])
+      ])
+
+      return documents?.users[0]
+    }
+  }
+}
